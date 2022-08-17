56a81ecb7eca1068c45164b001b590e171ae726d
Ran Zonis <zonisr@gmail.com>
2022-08-17 10:38:51 +0300

    anti diagonal bug fixed

--- Assets/Scripts/WinCondition.cs
+++ Assets/Scripts/WinCondition.cs
@@ -1,4 +1,6 @@
 ﻿
+using UnityEngine;
+
 namespace TicTacToe
 {
     public class WinCondition
@@ -14,12 +16,18 @@ namespace TicTacToe
             int row = markCommand.GridPosition.Row;
             int col = markCommand.GridPosition.Col;
 
+            Debug.Log("Testing mark for final: " + row + " " + col + " " + mark.ToString());
+            
+            Debug.Log("Test Col");
             if (IsMarkFullCol(col, mark))
                 return true;
-            if (IsMarkFullRow(col, mark))
+            Debug.Log("Test Row");
+            if (IsMarkFullRow(row, mark))
                 return true;
+            Debug.Log("Test Diag");
             if (IsMarkFullMainDiagonal(row, col, mark))
                 return true;
+            Debug.Log("Test AntiDiag");
             if (IsMarkFullAntiDiagonal(row, col, mark))
                 return true;
 
@@ -33,7 +41,6 @@ namespace TicTacToe
                 if (_gameGrid.GetSlotValue(row, i) != mark)
                     return false;
             }
-
             return true;
         }
 
@@ -64,12 +71,13 @@ namespace TicTacToe
 
         private bool IsMarkFullAntiDiagonal(int row, int col, Mark mark)
         {
-            if (row != _gameGrid.GridSize - col)
+            int lastIndex = _gameGrid.GridSize - 1;
+            if (row != lastIndex - col)
                 return false;
 
             for (int i = 0; i < _gameGrid.GridSize; i++)
             {
-                if (_gameGrid.GetSlotValue(i, _gameGrid.GridSize - i) != mark)
+                if (_gameGrid.GetSlotValue(i, lastIndex - i) != mark)
                     return false;
             }
 
